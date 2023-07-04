757d6f1af1f87a066c194eb7d4d3facab616135d
Christian <c.espinoza.code@gmail.com>
2023-07-04 10:15:27 -0500

    Main component added to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import './App.css';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { MainSection } from './components/MainSection';
 
 function App() {
   return (
     <>
       <Header />
-      <main>
-        <section></section>
-        <section></section>
-        <section></section>
-        <section></section>
-      </main>
+      <MainSection />
       <Footer />
     </>
   );
