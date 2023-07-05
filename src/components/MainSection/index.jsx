26bd28efc2e12b6a215f4d1420d68d062202575a
Christian <c.espinoza.code@gmail.com>
2023-07-05 10:17:08 -0500

    main content component created

---
+++ src/components/MainSection/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+function MainSection() {
+  return (
+    <main>
+      <section><p>s1</p></section>
+      <section><p>s2</p></section>
+      <section><p>s3</p></section>
+      <section><p>s4</p></section>
+    </main>
+  )
+}
+
+export { MainSection }
