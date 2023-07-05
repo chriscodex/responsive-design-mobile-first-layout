c690aee655f48a51585f14cf4889031a130443b1
Christian <c.espinoza.code@gmail.com>
2023-07-05 12:36:25 -0500

    colors added to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'bitcoin-orange': '#f7931a',
+      'soft-orange': '#ffe9d5',
+      'secondary-blue': '#1a9af7',
+      'soft-blue': '#e7f5ff',
+      'warm-black': '#282623',
+      'black': '#201e1c',
+      'grey': '#bababa',
+      'off-white': '#faf8f7',
+      'just-white': '#ffffff',
+    },
     extend: {},
   },
   plugins: [],
