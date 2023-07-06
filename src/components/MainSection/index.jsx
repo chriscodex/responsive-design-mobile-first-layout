6ca78d8e780f11da0a047f125a68820f2808bfe1
Christian <c.espinoza.code@gmail.com>
2023-07-05 19:28:05 -0500

    structure & classes for main section component added

--- src/components/MainSection/index.jsx
+++ src/components/MainSection/index.jsx
@@ -3,7 +3,15 @@ import './styles.css'
 function MainSection() {
   return (
     <main>
-      <section><p>s1</p></section>
+      <section className='main-exchange-cotainer'>
+        <div className='main-exchange-container__backgroundImg'></div>
+        <div className='main-exchange-container__title'></div>
+        <section className='main-exchange-container__tables'>
+          <div>
+
+          </div>
+        </section>
+      </section>
       <section><p>s2</p></section>
       <section><p>s3</p></section>
       <section><p>s4</p></section>
