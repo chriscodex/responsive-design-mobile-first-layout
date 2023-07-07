ff0690d147dd3c7a839fa8d999370d7f222079ab
Christian <c.espinoza.code@gmail.com>
2023-07-07 17:36:57 -0500

    main section html structure created

--- src/components/MainSection/index.jsx
+++ src/components/MainSection/index.jsx
@@ -10,8 +10,37 @@ function MainSection() {
           <p className='text-warm-grey'>Traemos toda la información de las tasas de cambio y las monedas más importantes del mundo</p>
         </div>
         <section className='main-exchange-container__tables'>
-          <div>
-
+          <div className='tableContainer'>
+            <h3>Monedas</h3>
+            <section className='tableContainer__table'>
+              <div>
+                <h4>Bitcoin</h4>
+              </div>
+              <div>
+                <h5>$1.96<span></span></h5>
+              </div>
+              <div>
+                <h4>Ethereum</h4>
+              </div>
+              <div>
+                <h5>$0.07<span></span></h5>
+              </div>
+              <div>
+                <h4>Ripple</h4>
+              </div>
+              <div>
+                <h5>$2.15<span></span></h5>
+              </div>
+              <div>
+                <h4>Stellar</h4>
+              </div>
+              <div>
+                <h5>$4.96<span></span></h5>
+              </div>
+            </section>
+            <div className='tableContainer__updateDate'>
+              <p><span>Actualizado: </span>07 Julio 5:13</p>
+            </div>
           </div>
         </section>
       </section>
