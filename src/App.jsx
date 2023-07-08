ae50e15b2e1eb8c4bcd7177377010deb5c7da1d1
Christian <c.espinoza.code@gmail.com>
2023-07-07 19:08:01 -0500

    Products component added to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { MainSection } from './components/MainSection';
+import { Products } from './components/Products';
 
 function App() {
   return (
     <>
       <Header />
       <MainSection />
+      <Products />
       <Footer />
     </>
   );
