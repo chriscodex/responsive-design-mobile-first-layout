6e96caeb6b3303629ca6dfa4648a16e46b3b9ce9
Christian <c.espinoza.code@gmail.com>
2023-07-08 15:10:41 -0500

    Plan card component height error fixed

--- src/components/Plans/index.jsx
+++ src/components/Plans/index.jsx
@@ -1,26 +1,47 @@
 import './styles.css';
 import { PlanCardRecomendado } from './PlanCardRecomendado';
+import { PlanCard } from './PlanCard';
 
 function Plans() {
   const planContent = [
+    {
+      title: 'Pago Mensual',
+      price: '19',
+      description: 'Plan Básico.',
+    },
     {
       title: 'Pago Anual',
       price: '99',
       description: '* Ahorras $129 comparado al plan mensual.',
     },
+    {
+      title: 'De por vida',
+      price: '199',
+      description: 'Plan Ilimitado.',
+    },
   ];
 
   return (
     <section className="plans-container">
-      <div className='plans-container__title'>
+      <div className="plans-container__title">
         <h2>Escoge el plan que mejor se ajuste a ti.</h2>
         <p>Cualquier plan te da acceso completo a nuestra plataforma.</p>
       </div>
-      <section className="plans-container__slice">
-        <PlanCardRecomendado
+      <section className="plans-container__slide">
+        <PlanCard
           title={planContent[0].title}
           price={planContent[0].price}
           description={planContent[0].description}
+        />
+        <PlanCardRecomendado
+          title={planContent[1].title}
+          price={planContent[1].price}
+          description={planContent[1].description}
+        />
+        <PlanCard
+          title={planContent[2].title}
+          price={planContent[2].price}
+          description={planContent[2].description}
         />
       </section>
     </section>
