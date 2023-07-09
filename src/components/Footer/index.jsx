39af4a48aed0080cacffba8deeabeee8225dbaf3
Christian <c.espinoza.code@gmail.com>
2023-07-08 19:23:30 -0500

    footer html structure created

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,19 @@
-import './styles.css'
+import './styles.css';
+import logoFooter from '../../assets/img/logo-footer.svg'
 
 function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="footer-container">
+      <div className='footer-container__social-media'>
+        <a href="#">LinkedIn</a>
+        <a href="#">Crunchbase</a>
+        <a href="#">Hackernews</a>
+      </div>
+      <div className='footer-container__logo'>
+        <img src={logoFooter} alt="batatabit-logo" />
+      </div>
     </footer>
-  )
+  );
 }
 
-export { Footer }
+export { Footer };
